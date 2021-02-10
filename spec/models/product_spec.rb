590fc1241d98cb5cda61a6a778066bf06403bbc8
Maksym Tesliuk <maksym.tesliuk@ext.chime.com>
2021-02-10 16:16:54 +0200

    Add some coverage

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without a title" do
+    product = Product.new(name: nil)
+    expect(product).to_not be_valid
+  end
+
+  it "is not valid without a price" do
+    product = Product.new(price: nil)
+    expect(product).to_not be_valid
+  end
+
+  it "is not valid without a inward_date" do
+    product = Product.new(inward_date: nil)
+    expect(product).to_not be_valid
+  end
+
+  it "is not valid without a type" do
+    product = Product.new(type: nil)
+    expect(product).to_not be_valid
+  end
 end
