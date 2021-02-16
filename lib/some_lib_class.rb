408b9b4d12c4a7b74c7c23a1475df6faae55740c
Maksym Tesliuk <maksym.tesliuk@ext.chime.com>
2021-02-16 12:35:50 +0200

    Add ugly code and vulnerability

--- lib/some_lib_class.rb
+++ lib/some_lib_class.rb
@@ -13,6 +13,20 @@ class SomeLibClass
   end
 
   def hello_from_too_long_method_name
+    'i'
+    'make'
+    'this'
+    'method'
+    'longer'
+    and_use = this_method_for_fun('hehe')
     return "hello there"
+  end
+
+  def this_method_for_fun(laugh)
+    unused_variable_here = 'test'
+
+    #
+    # very very redundant return
+    return laugh
   end
 end
